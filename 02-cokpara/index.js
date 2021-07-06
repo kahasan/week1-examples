cb6407fe968e6e7dc030340161392ca710cf95c0
kahasan <kahraman.h@outlook.com>
2021-07-06 23:27:08 +0300

    Edit indexjs and add css

--- 02-cokpara/index.js
+++ 02-cokpara/index.js
@@ -1,1 +1,94 @@
-console.log('mounted');
+import products from './productList.js';
+
+const container = document.querySelector('#container');
+
+let BILL_GATES_MONEY = Number.MAX_SAFE_INTEGER;
+
+let adet = 0;
+let alinanAdet = 0;
+
+const moneyTitle = document.createElement('h1');
+moneyTitle.textContent = 'Kalan Para';
+moneyTitle.className = 'money-title';
+
+const moneySection = document.createElement('div');
+moneySection.className = 'money';
+moneySection.appendChild(moneyTitle);
+
+const money = document.createElement('h1');
+money.textContent = String(BILL_GATES_MONEY);
+moneySection.appendChild(money);
+
+const deneme = (event) => {
+  const price = parseInt(event.path[1].childNodes[2].childNodes[1].textContent);
+  money.textContent = BILL_GATES_MONEY;
+  adet = parseInt(BILL_GATES_MONEY / price);
+  event.path[1].childNodes[4].attributes[1].textContent = `Bu üründen ${adet} adet alabilirsiniz`;
+
+  if (BILL_GATES_MONEY > 0 && BILL_GATES_MONEY - price >= 0) {
+    BILL_GATES_MONEY = BILL_GATES_MONEY - price;
+    event.path[1].childNodes[3].childNodes[1].textContent =
+      parseInt(event.path[1].childNodes[3].childNodes[1].textContent) + 1;
+  } else {
+    alert('Maalesef paranız yetmiyor.. :(');
+  }
+};
+
+products.map((product) => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const title = document.createElement('h3');
+  title.className = 'title';
+  title.textContent = product.name;
+
+  const imgDiv = document.createElement('div');
+  imgDiv.className = 'img-div';
+
+  const img = document.createElement('img');
+  img.src = product.img;
+
+  const priceSection = document.createElement('div');
+  priceSection.className = 'price-section';
+
+  const priceTitle = document.createElement('div');
+  priceTitle.textContent = 'Fiyat';
+
+  const price = document.createElement('div');
+  price.className = 'price';
+  price.textContent = product.price;
+
+  priceSection.appendChild(priceTitle);
+  priceSection.appendChild(price);
+
+  const pieceSection = document.createElement('div');
+  pieceSection.className = 'piece-section';
+
+  const pieceTitle = document.createElement('div');
+  pieceTitle.textContent = 'Alınan adet';
+
+  const piece = document.createElement('div');
+  piece.className = 'amount';
+  piece.textContent = parseInt(alinanAdet);
+
+  pieceSection.appendChild(pieceTitle);
+  pieceSection.appendChild(piece);
+
+  const btn = document.createElement('button');
+  btn.textContent = 'Satın Al';
+  btn.className = 'btn';
+  btn.addEventListener('click', deneme);
+  btn.title = `Bu üründen ${
+    BILL_GATES_MONEY / product.price
+  } adet alabilirsiniz`;
+
+  container.appendChild(card);
+  card.append(title);
+  card.appendChild(imgDiv);
+  imgDiv.appendChild(img);
+  card.appendChild(priceSection);
+  card.appendChild(pieceSection);
+  card.appendChild(btn);
+});
+
+document.body.appendChild(moneySection);
---
+++ 02-cokpara/productList.js
@@ -0,0 +1,53 @@
+const productList = [
+  {
+    id: 1,
+    name: 'Kola',
+    price: 4,
+    img: 'https://i.sozcu.com.tr/wp-content/uploads/2016/01/19/diyet-kola.jpg',
+  },
+  {
+    id: 2,
+    name: 'Iskender',
+    price: 30,
+    img: 'https://i.lezzet.com.tr/images-xxlarge-recipe/ev-yapimi-iskender-33bd7089-fa36-4398-95f8-02c6463ea27c.jpg',
+  },
+  {
+    id: 3,
+    name: 'Yat',
+    price: 450000,
+    img: 'https://i.ytimg.com/vi/9BCZpcgsAb8/maxresdefault.jpg',
+  },
+  {
+    id: 4,
+    name: 'Bahceli Ev',
+    price: 9500000,
+    img: 'https://www.neredekal.com/res/blog/1582812421_7.jpg',
+  },
+  {
+    id: 5,
+    name: 'Araba Fabrikası',
+    price: 120000000,
+    img: 'https://i.ytimg.com/vi/rfMkp55oTv0/maxresdefault.jpg',
+  },
+  {
+    id: 6,
+    name: 'Roket',
+    price: 500000000,
+    img: 'https://bilimgenc.tubitak.gov.tr/sites/default/files/158584803_roket_motor_small123.jpg',
+  },
+  {
+    id: 7,
+    name: 'Lüks Ev',
+    price: 65500000000,
+    img: 'https://www.webtekno.com/images/editor/default/0001/60/bdf725662d057e23e11594e1ae9fb949a329d0a3.jpeg',
+  },
+  {
+    id: 8,
+    name: "TR'de benzin",
+    price: 99999999999999,
+    img: 'https://i2.milimaj.com/i/milliyet/75/0x410/5e6b2a6555428014542b964b.jpg',
+  },
+  // ... Kendi örneklerinizi eklemeye çekinmeyin.
+];
+
+export default productList;
