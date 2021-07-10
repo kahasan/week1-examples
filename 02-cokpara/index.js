f8b33ec701db292b1b9fcd1f822daea2d079bb9b
kahasan <kahraman.h@outlook.com>
2021-07-10 12:08:25 +0300

    Add hover section to show purchasable amount

--- 02-cokpara/index.js
+++ 02-cokpara/index.js
@@ -4,8 +4,9 @@ const container = document.querySelector('#container');
 
 let BILL_GATES_MONEY = Number.MAX_SAFE_INTEGER;
 
-let adet = 0;
-let alinanAdet = 0;
+//let adet = 0;
+//let alinanAdet = 0;
+let purchasable = 0;
 
 const moneyTitle = document.createElement('h1');
 moneyTitle.textContent = 'Kalan Para';
@@ -19,16 +20,19 @@ const money = document.createElement('h1');
 money.textContent = String(BILL_GATES_MONEY);
 moneySection.appendChild(money);
 
-const deneme = (event) => {
+const calc = (event) => {
   const id = event.target.id;
   const price = parseInt(document.querySelector(`#price-${id}`).textContent);
 
   const amount = document.querySelector(`#amount-${id}`);
-  console.log(amount);
+
+  const purchasableTag = document.querySelector(`#purchasable-${id}`);
 
   money.textContent = BILL_GATES_MONEY;
-  // adet = parseInt(BILL_GATES_MONEY / price);
-  //event.path[1].childNodes[4].attributes[1].textContent = `Bu üründen ${adet} adet alabilirsiniz`;
+
+  purchasable = parseInt(BILL_GATES_MONEY / price);
+
+  purchasableTag.textContent = `${purchasable} adet alınabilir`;
 
   if (BILL_GATES_MONEY > 0 && BILL_GATES_MONEY - price >= 0) {
     BILL_GATES_MONEY = BILL_GATES_MONEY - price;
@@ -46,17 +50,30 @@ products.map((product) => {
                             </div>
                             <div class="price-section">
                               <div>Fiyat</div>
-                              <div class="price" id="price-${product.id}">${product.price}</div>
+                              <div class="price" id="price-${product.id}">${
+    product.price
+  }</div>
                             </div>
                             <div class="piece-section">
                               <div>Alınan adet</div>
-                              <div class="amount" id="amount-${product.id}">${alinanAdet}</div>
+                              <div class="amount" id="amount-${
+                                product.id
+                              }">${0}</div>
                             </div>
-                            <button class="btn" id=${product.id}>Satın Al</button>
+                            <div class="action">
+                            <button class="btn" id=${
+                              product.id
+                            }>Satın Al</button> 
+                            <div class="purchasable" id="purchasable-${
+                              product.id
+                            }">${
+    !purchasable && parseInt(BILL_GATES_MONEY / product.price)
+  } adet alınabilir</div>
+                            </div>
                           </div>`;
 
   document.querySelectorAll('.btn').forEach((item) => {
-    item.addEventListener('click', deneme);
+    item.addEventListener('click', calc);
   });
 });
 
